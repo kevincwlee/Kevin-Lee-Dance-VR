ff32f105bb85bc1371b1f47f2d9b10dfd1b39da9
kevincwlee <60997590+kevincwlee@users.noreply.github.com>
2020-05-08 19:37:55 -0400

    Almost Final Version
    Recreated the scene after last Unity project crashed and wouldn't load any of my old scripts. Reimported all my old assets and added more to the scene in terms of particle effects, new hand models for the controllers (figured that part out), and also fixed the color range of my MotionColor script so that it wouldn't stay on white majority of the time. Lastly, I also made some speaker models out of Unity prefabs and am about to start on creating instruments (sample-based) that the player can trigger when they hit the object with the keytar.

---
+++ Assets/My Scripts/HeadFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadFilter : MonoBehaviour
+{
+    Transform HeadTransform;
+    AudioLowPassFilter AudioFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HeadTransform = gameObject.transform;
+        AudioFilter = gameObject.GetComponent<AudioLowPassFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("x: " + HeadTransform.rotation.x + " y: " + HeadTransform.rotation.y + " z: " + HeadTransform.rotation.z + " filter: " + AudioFilter.cutoffFrequency);
+        AudioFilter.cutoffFrequency = (11000*(HeadTransform.rotation.x + 1)/2);
+    }
+}
---
+++ Assets/My Scripts/MotionColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionColor : MonoBehaviour
+{
+    Material CubeColor;
+    Transform HeadCam;
+
+
+    void Awake()
+    {
+        CubeColor = GetComponent<MeshRenderer>().material;
+        HeadCam = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("EulerAngles: " + HeadCam.rotation.eulerAngles);
+        Vector3 rot = HeadCam.rotation.eulerAngles;
+        CubeColor.color = new Color((rot.x%360)/360, (rot.y%360)/360, (rot.z%360)/360);
+        
+    }
+}
