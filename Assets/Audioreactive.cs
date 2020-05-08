3f884b97c9df263a99b9ee5636f096e462da6de4
kevincwlee <60997590+kevincwlee@users.noreply.github.com>
2020-05-08 19:31:23 -0400

    Not sure what rendition but likely this crashed my file (started again)

---
+++ Assets/Audioreactive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBounce : MonoBehaviour
+{
+    AudioSource AudioDB;
+    Transform ObjectScale;
+
+
+    void Awake()
+    {
+        AudioDB = GetComponent<AudioSource>();
+        ObjectScale = gameObject.GetComponent<Transform>();
+        float[] samples = new float[AudioDB.clip.samples * AudioDB.clip.channels];
+        AudioDB.clip.GetData(samples, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/MotionColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionColor : MonoBehaviour
+{
+    Material CubeColor;
+    Transform HeadCam;
+
+
+    void Awake()
+    {
+        CubeColor = GetComponent<MeshRenderer>().material;
+        HeadCam = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("EulerAngles: " + HeadCam.rotation.eulerAngles);
+        Vector3 rot = HeadCam.rotation.eulerAngles;
+        CubeColor.color = new Color(rot.x/10, rot.y/10, rot.z/10);
+        
+    }
+}
